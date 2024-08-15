f5b4838d83ba58d6113fb6528c2e75136f5fd235
augustjourney <53155077+augustjourney@users.noreply.github.com>
2024-08-16 00:31:55 +0600

    added errs pkg

---
+++ pkg/errs/errors.go
@@ -0,0 +1,34 @@
+package errs
+
+import (
+	"errors"
+)
+
+var (
+	// general
+
+	ErrInternal = errors.New("internal error")
+	ErrNotFound = errors.New("not found")
+
+	// users
+
+	ErrUserAlreadyExists = errors.New("user already exists")
+	ErrUserNotFound      = errors.New("user not found")
+
+	// auth
+
+	ErrEmptyUsername    = errors.New("username cannot be empty")
+	ErrEmptyPassword    = errors.New("password cannot be empty")
+	ErrShortPassword    = errors.New("password should be at least 8 characters")
+	ErrEmptyEmail       = errors.New("email cannot be empty")
+	ErrEmptyFirstName   = errors.New("first name cannot be empty")
+	ErrWrongCredentials = errors.New("wrong login or password")
+
+	// images
+
+	ErrNoImageForUploading = errors.New("no image provided for upload")
+
+	// handler
+
+	ErrNoMultipartFormData = errors.New("file in multipart form data not provided")
+)
